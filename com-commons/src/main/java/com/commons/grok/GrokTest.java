68c0879716c20405ae64d9276baad3cdd21eb049
yangzhi05 <yangzhi05@qiyi.com>
2025-09-11 17:17:03 +0800

    添加readme

--- com-commons/src/main/java/com/commons/grok/GrokTest.java
+++ com-commons/src/main/java/com/commons/grok/GrokTest.java
@@ -6,8 +6,17 @@ import io.krakens.grok.api.GrokCompiler;
 import io.krakens.grok.api.Match;
 import java.util.Map;
 
+/**
+ * io.krakens:java-grok 是一个 Java 库，用于解析和匹配非结构化的日志数据，将其转换为结构化的键值对格式。
+ * 这个库基于流行的 Grok 模式，最初由 Elasticsearch 的 Logstash项目推广使用。
+ * <dependency>
+ *      <groupId>io.krakens</groupId>
+ *      <artifactId>java-grok</artifactId>
+ *      <version>0.1.9</version>
+ * </dependency>
+ */
 public class GrokTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /* Create a new grokCompiler instance */
         GrokCompiler grokCompiler = GrokCompiler.newInstance();
         grokCompiler.registerDefaultPatterns();
@@ -16,7 +25,8 @@ public class GrokTest {
         final Grok grok = grokCompiler.compile("%{COMBINEDAPACHELOG}");
 
         /* Line of log to match */
-        String log = "112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"";
+        String log =
+                "112.169.19.192 - - [06/Mar/2013:01:36:30 +0900] \"GET / HTTP/1.1\" 200 44346 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.152 Safari/537.22\"";
         Match gm = grok.match(log);
         /* Get the map with matches */
         final Map<String, Object> capture = gm.capture();
