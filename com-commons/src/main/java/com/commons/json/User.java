ee32c38f75b064f3e327e1ad37c8b25591ae89b6
yangzhi05 <yangzhi05@qiyi.com>
2025-09-29 17:27:38 +0800

    补充readme

--- com-commons/src/main/java/com/commons/json/User.java
+++ com-commons/src/main/java/com/commons/json/User.java
@@ -5,6 +5,15 @@ import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * @JSONField(ordinal=1)//配置序列化的字段顺序（1.1.42版本之后才支持）
+ * @JSONField(serialize=false) //是否参与序列化：该字段不输出  但是如果加了final，这个字段就无法被过滤
+ * @JSONField(derialize=false) //是否参与反序列化：该字段不输出  但是如果加了final，这个字段就无法被过滤
+ * @JSONField(format="yyyy-MM-dd HH:mm:ss") //日期按照指定格式序列化
+ * @JSONField(name="别名"); //使用字段别名
+ * @JSONField(serialzeFeatures={SerialzeFeatures属性}); //序列化规则
+ * @JSONField(parseFeatures={Features属性}); //反序列化规则
+ */
 @Data
 @AllArgsConstructor
 public class User {
