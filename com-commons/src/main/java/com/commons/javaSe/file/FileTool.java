8fcb653545d236cba05dd1133c6d57bab559b647
yangzhi05 <yangzhi05@qiyi.com>
2025-11-07 11:54:32 +0800

    补充readme

--- com-commons/src/main/java/com/commons/javaSe/file/FileTool.java
+++ com-commons/src/main/java/com/commons/javaSe/file/FileTool.java
@@ -14,13 +14,12 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class FileTool {
-	/**
-	 * 关键点说明
-	 * BufferedReader.readLine():
-	 * 		按行读取时，会去掉每一行的换行符，返回一个字符串，直到文件结束返回 null。
-	 * 关闭资源:
-	 * 		使用 try-with-resources 自动管理资源，保证在程序运行结束后会安全地关闭文件流，避免资源泄露。
-	 */
+
+	// 关键点说明
+	// BufferedReader.readLine():
+	// 		按行读取时，会去掉每一行的换行符，返回一个字符串，直到文件结束返回 null。
+	// 关闭资源:
+	// 		使用 try-with-resources 自动管理资源，保证在程序运行结束后会安全地关闭文件流，避免资源泄露。
 	public List<String> readFileByBufferedReader(String filePath) {
 		List<String> lines = new ArrayList<>();
 		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -35,15 +34,13 @@ public class FileTool {
 		return lines;
 	}
 
-	/**
-	 * 关键点说明
-	 * hasNextLine() 方法:
-	 * 		判断文件中是否有下一行可读内容。
-	 * 应用场景:
-	 * 		适用于小文件，或文件内容按行解析的场景。
-	 * 自动关闭资源:
-	 * 		通过 try-with-resources 自动关闭 Scanner。
-	 */
+	// 关键点说明
+	// hasNextLine() 方法:
+	//  	判断文件中是否有下一行可读内容。
+	// 应用场景:
+	//  	适用于小文件，或文件内容按行解析的场景。
+	// 自动关闭资源:
+	//  	通过 try-with-resources 自动关闭 Scanner。
 	public List<String> readFileByScanner(String filePath) {
 		List<String> lines = new ArrayList<>();
 		try (Scanner scanner = new Scanner(new File(filePath))) {
@@ -56,7 +53,8 @@ public class FileTool {
 		}
 		return lines;
 	}
-	//scanner通过close关闭
+
+	// scanner通过close关闭
 	public List<String> readFileByScanner2(String filePath) throws FileNotFoundException {
 		List<String> lines = new ArrayList<>();
 		Scanner scanner = new Scanner(new File(filePath));
@@ -68,15 +66,13 @@ public class FileTool {
 		return lines;
 	}
 
-	/**
-	 * 关键点说明
-	 * Files.lines():
-	 * 		自动处理文件的读取，返回一个流（Stream<String>），支持流式操作。
-	 * 性能:
-	 * 		Files.lines()非常适合用于处理大文件（在懒加载的模式下逐行读取，而不是一次性将文件全部加载到内存中）。
-	 * 自动关闭流:
-	 * 		通过 try-with-resources，Stream 在使用完之后会自动关闭，与文件释放关联资源。
-	 */
+	// 关键点说明
+	// Files.lines():
+	// 		自动处理文件的读取，返回一个流（Stream<String>），支持流式操作。
+	// 性能:
+	// 		Files.lines()非常适合用于处理大文件（在懒加载的模式下逐行读取，而不是一次性将文件全部加载到内存中）。
+	// 自动关闭流:
+	// 		通过 try-with-resources，Stream 在使用完之后会自动关闭，与文件释放关联资源。
 	public List<String> readFileByNio(String filePath) {
 		List<String> lines = new ArrayList<>();
 		try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
@@ -87,9 +83,7 @@ public class FileTool {
 		return lines;
 	}
 
-	/**
-	 * 用于读取文件并一次性将所有行读取到内存中
-	 */
+	// 用于读取文件并一次性将所有行读取到内存中
 	public List<String> readFileByNioAll(String filePath) throws IOException {
 		return Files.readAllLines(Paths.get(filePath), StandardCharsets.UTF_8);
 	}
