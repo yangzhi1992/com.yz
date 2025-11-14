17a04c5f18eefe06cd02182d3ac21b292d65da77
yangzhi05 <yangzhi05@qiyi.com>
2025-11-14 11:17:18 +0800

    补充readme

---
+++ com-camel/src/main/java/com.commons.camel/CamelMainApplication.java
@@ -0,0 +1,32 @@
+package com.commons.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.springframework.boot.SpringApplication;
+
+public class CamelMainApplication {
+	public static void main(String[] args) throws Exception {
+		// 创建 CamelContext 实例
+		CamelContext camelContext = new DefaultCamelContext();
+		camelContext.addRoutes(new MyRoute());
+		// 启动 CamelContext
+		camelContext.start();
+
+		Thread.currentThread().join();
+		// 关闭 CamelContext
+		camelContext.stop();
+	}
+
+	// 定义 Camel 路由
+	static class MyRoute extends RouteBuilder {
+		@Override
+		public void configure() {
+			// 定时器，每 1 秒执行一次，打印 "Hello World!" 到控制台
+			from("timer:hello?period=1000")
+					.setBody()
+					.constant("main Hello World!") // 设置消息的内容为 "Hello World!"
+					.to("stream:out"); // 将消息输出到控制台
+		}
+	}
+}
